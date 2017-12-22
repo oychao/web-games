785f1af4fb2d1a01a45b96f92a4860e376ea0572
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-12-22 19:18:44 +0800

    use 4 spaces as indent

--- mini-requirejs/index.js
+++ mini-requirejs/index.js
@@ -1,25 +1,25 @@
 const require = (path) => {
-  const module = require.module[path];
-  if (!module) {
-    throw new Error(`Module ${path} not registered.`);
-  }
-  if (module.exports) {
+    const module = require.module[path];
+    if (!module) {
+        throw new Error(`Module ${path} not registered.`);
+    }
+    if (module.exports) {
+        return module.exports;
+    }
+    module.exports = {};
+    module.func.call(null, module, module.exports, require);
     return module.exports;
-  }
-  module.exports = {};
-  module.func.call(null, module, module.exports, require);
-  return module.exports;
 };
 
 require.module = {};
 
 require.register = (path, func) => {
-  require.module[path] = {
-    func,
-    exports: null
-  };
+    require.module[path] = {
+        func,
+        exports: null
+    };
 };
 
 require.init = fn => {
-  fn.call(null, require);
+    fn.call(null, require);
 };
